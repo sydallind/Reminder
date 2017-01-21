f087b97f9844c3e5da95efe0b5ba52130a11bf51
Andrei Palangean <palangeanad@yahoo.com>
2017-01-21 14:56:09 +0200

    error file

--- Reminder/Errors.h
+++ Reminder/Errors.h
@@ -2,6 +2,12 @@
 
 #define S_SUCCES 0
 
-#define ERR_ALLOC 1
-#define ERR_OVERFLOW 2
-#define ERR_NOT_FOUND 3
+#define ERR_OFFSET 100
+#define ERR_ALLOC 101
+#define ERR_DATE 102
+#define ERR_HOUR 103
+#define ERR_NOT_FOUND 104
+#define ERR_PHONE_NR 105
+#define ERR_CALL 106
+#define ERR_MEETING 107
+#define ERR_UNKNOWN 999

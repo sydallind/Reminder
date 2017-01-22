bfb1c720817f106b24730893de3c280239d0340f
Andrei Palangean <palangeanad@yahoo.com>
2017-01-22 22:50:07 +0200

    send

--- Reminder/SmartReminder.h
+++ Reminder/SmartReminder.h
@@ -1,6 +1,16 @@
 #pragma once
+#include"Nota.h"
 
 class cSmartReminder
 {
+	private:
+		cNota *Note[500];
+		int nrnote;
+		int ora_inc,ora_sf;
+	public:
+		cSmartReminder();
+		~cSmartReminder();
+		void display(int d = 1, int h = 0);
+		void addSedinta(float dur, char subiect[]);
 
 };
